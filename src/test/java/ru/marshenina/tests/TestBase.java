e9b4e819a1f97b10aa4b770c2808fe33e242c6d4
Valeriia Marshenina <valeriamarshenina@yandex.ru>
2023-04-30 18:45:04 +0400

    Refactored code

--- src/test/java/ru/marshenina/tests/TestBase.java
+++ src/test/java/ru/marshenina/tests/TestBase.java
@@ -15,11 +15,6 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "100.0";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
